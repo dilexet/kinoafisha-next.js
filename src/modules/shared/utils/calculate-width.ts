27ac505cb9566a839e0396d4f7b5df632be4ac52
Pavarha Maksim <dilexet@gmail.com>
2023-02-16 16:15:52 +0300

    change calculation width

--- src/modules/shared/utils/calculate-width.ts
+++ src/modules/shared/utils/calculate-width.ts
@@ -1,4 +1,4 @@
 export function calculateWidthBySeats(seatsLength: number): string {
   const width = seatsLength * 50 + 50;
-  return `${width}px`;
+  return `${width > window.innerWidth ? window.innerWidth * 0.8 : width}px`;
 }
